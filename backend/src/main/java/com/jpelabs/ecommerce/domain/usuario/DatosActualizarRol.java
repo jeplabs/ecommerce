5fc830ed1fa93d8d600afd6cadbbf863c3ece438
Edwin Alejandro Anona <alexedwin1@gmail.com>
2026-02-22 08:01:32 -0600

    Subir correctamente carpeta alternativa ecommerce

--- backend/src/main/java/com/jpelabs/ecommerce/domain/usuario/DatosActualizarRol.java
+++
@@ -1,8 +0,0 @@
-package com.jpelabs.ecommerce.domain.usuario;
-
-import jakarta.validation.constraints.NotNull;
-
-public record DatosActualizarRol(
-        @NotNull(message = "El rol es obligatorio")
-        Rol rol
-) {}
