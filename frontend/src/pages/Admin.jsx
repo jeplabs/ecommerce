ecce9c2e9ebd3c5f816fbba935ea8da5fda11d7b
Pablo Lambert <pablomartin.lambert@gmail.com>
2026-02-28 03:04:46 -0300

    admin comienzo

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,10 +1,36 @@
 import Navbar from "../components/Navbar"
 
 export default function Admin() {
+
+    const listaUsuarios = async () => {
+        try {
+            const response = await fetch('http://localhost:8081/api/auth/usuarios', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+            throw new Error('Credenciales inválidas');
+            }
+
+            const data = await response.json();
+            
+            console.log(data);
+        } catch (error) {
+            // Muestra mensaje de error al usuario
+            console.error('Error', error);
+            setError('Credenciales inválidas');
+        }
+    };
+
     return (
         <>
             <Navbar />
             <h1>Admin</h1>
+            <button onClick={listaUsuarios}>Lista de usuarios</button>
+
         </>
     )
 }
