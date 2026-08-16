cebd24fb70a44bad0f1eeeb9a7f74ac9c7143f6a
Edwin Alejandro Anona <alexedwin1@gmail.com>
2026-08-16 12:20:21 -0600

    fix(cors configuration): configuración de CORS en Spring Security 6 / Spring Boot 3

--- backend/src/main/java/com/jeplabs/ecommerce/infra/security/SecurityConfigurations.java
+++ backend/src/main/java/com/jeplabs/ecommerce/infra/security/SecurityConfigurations.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.*;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,7 +36,7 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(cors -> cors.configure(http))
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> req
